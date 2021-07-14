ae48693ce03dbe1778366f9cdb277a901889a45d
wscreativefruits <87406947+wscreativefruits@users.noreply.github.com>
2021-07-14 14:13:53 -0400

    Add files via upload

---
+++ about.php
@@ -0,0 +1,16 @@
+<?php
+   include_once 'header.php';
+?>
+
+		<div class="about_wapper">
+			<ul class="about_links">
+				<li class="group4"><h2>W.S Creative fruits is artistic fruit designs. Presentation
+						       symbolises the amount of effort you put in for the other party.
+						       Additionally, it portrays the amount of respect you are giving.
+						       So, why not an a universal gift of a fruit basket?</h2>
+				</li>
+				
+			</ul> 
+		</div>	
+	</body>
+</html>
---
+++ social.php
@@ -0,0 +1,10 @@
+<?php
+   include_once 'header.php';
+?>
+			<ul class="social_links">
+				<li class="group3" style="align-self: end;"><a href="https://www.instagram.com/w.s_creative_fruits/">Instagram</a></li>
+				<li class="group3"><a href="https://www.facebook.com/WendysCreative/">Facebook</a></li>
+			</ul>
+		
+	</body>
+</html>
